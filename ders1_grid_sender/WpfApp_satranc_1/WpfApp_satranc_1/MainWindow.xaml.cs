46c8f6a09ae484d3cd3b773ecde12a917a20045c
ozgehalilduman <ozgehalilduman@gmail.com>
2014-12-03 07:26:11 +0200

    Daha açıklamalı hale getirmeye çalıştım-HALA ÇOK EKSİĞİ VAR

--- ders1_grid_sender/WpfApp_satranc_1/WpfApp_satranc_1/MainWindow.xaml.cs
+++ ders1_grid_sender/WpfApp_satranc_1/WpfApp_satranc_1/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace WpfApp_satranc_1
     /// </summary>
     public partial class MainWindow : Window
     {
-        Button hareketden_tas;
+        Button hareketden_tas=new Button();
         public MainWindow()
         {
             InitializeComponent();
@@ -28,17 +28,71 @@ namespace WpfApp_satranc_1
 
         private void eylem_click(object sender, RoutedEventArgs e)
         {
-            Brush tas_over;//üstüne gelinen tas
-            object tas_over_degeri;//ustune gelinen tas konumundaki tasın degeri
+            byte islem = 0;
             Button tas = (Button)sender;
+            if(hareketden_tas.Tag==null && tas.Tag==null)  
+            {
+                MessageBox.Show("HAREKET GERÇEKLEŞMEMELİ");
+            }
+            if (hareketden_tas.Tag == null && tas.Tag != null) 
+            { //ilk tasın seçildiği kısım
+                islem = 1;
+            }
+            if (hareketden_tas.Tag != null && tas.Tag == null)
+            { //hareket için seçilen tus bos bir alana hareket ediyor
+                islem = 2;         
+            }
+            if (hareketden_tas.Tag != null && tas.Tag != null)
+            { //ilk tasın seçildiği kısım
+                islem = 3;
+            }
+            switch(islem){
+                case 1: 
+                    btn_hareketeden.Background = hareketden_tas.Background;
+                    btn_secilen.Background = tas.Background;
+                    MessageBox.Show("HAREKET EDECEK TAŞIN SEÇİMİ GERÇEKLEŞTİ");
+                    
+                    hareketden_tas = tas;                    
+                    break;
+                case 2: 
+                    btn_hareketeden.Background = hareketden_tas.Background;
+                    btn_secilen.Background = tas.Background;
+                    MessageBox.Show("HAREKET GERÇEKLEŞİYOR");                    
+                    tas = hareketden_tas;
+                    btn_hareketeden.Background = hareketden_tas.Background;
+                    btn_secilen.Background = tas.Background;
+                    hareketEt(tas,0);
+                    MessageBox.Show("background yer değiştirdi");
+                    btn_hareketeden.Background = hareketden_tas.Background;
+                    btn_secilen.Background = tas.Background;
+                    break;
+                case 3: 
+                    MessageBox.Show("HAREKET EDEN TAŞ BAŞKA BİR TAŞIN ÜZERİNE HAREKET ETTİ");
+                    tas = hareketden_tas;
+                    hareketEt(tas,1);
+                    break;            
+            }            
+            
+        }
+        private void hareketEt(Button tas,byte eylem)
+        {
+            Brush brush_over;//üstüne gelinen tasın brush ı
+            Object tag_over;//üstüne gelinen tasın tag ı
 
-            tas_over_degeri = tas.Tag;
+            brush_over = tas.Background;            
+            tas.Background = hareketden_tas.Background;         
+
+            tag_over = tas.Tag;
             tas.Tag = hareketden_tas.Tag;
-            hareketden_tas.Tag = tas_over_degeri;
-
-            tas_over = tas.Background;
-            tas.Background = hareketden_tas.Background;            
-            hareketden_tas.Background = tas_over;
+            if(eylem==0)
+            {//bos bir alana hareket
+                hareketden_tas.Background = brush_over;
+                hareketden_tas.Tag = tag_over;
+            }
+            if (eylem == 1)
+            {//dolu bir tasın üzerine geldi.
+                
+            }
             
         }
     }
